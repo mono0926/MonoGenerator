28c59fff78496a9dcfac4faa99b035ec57461f8d
Masayuki Ono <mono0926@gmail.com>
2016-11-16 21:33:08 +0900

    Create main func

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,11 @@
-print("Hello, world!")
+private func main(arguments: [String]) {
+    let arguments = arguments.dropFirst()
+    guard let input = arguments.first else {
+        print("inputの引数を入力してください😡")
+        return
+    }
+    let generator = Generator(value: input)
+    print(generator.generate())
+}
+
+main(arguments: CommandLine.arguments)
