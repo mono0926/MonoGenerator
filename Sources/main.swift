85702820aa59504692f7ea48ac848725de3c4867
Masayuki Ono <mono0926@gmail.com>
2016-11-17 12:13:15 +0900

    Format

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,7 +3,7 @@ import Commander
 let main = command(Argument<String>("input"),
                    Option("suffix", "(　´･‿･｀)", flag: "s"),
                    Option("length", -1, flag: "l")) { input, suffix, length in
-    let generator = Generator(value: input)
+                    let generator = Generator(value: input)
                     print(generator.generate(suffix: suffix, maxLength: length < 0 ? nil : length))
 }
 
