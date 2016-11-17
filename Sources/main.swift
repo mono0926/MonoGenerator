b64b5c0e2d45b4b54d16f58d7021011408bc54a8
Masayuki Ono <mono0926@gmail.com>
2016-11-17 12:11:32 +0900

    Refactor

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,8 +1,8 @@
 import Commander
 
-let main = command(Argument("input"),
+let main = command(Argument<String>("input"),
                    Option("suffix", "(　´･‿･｀)", flag: "s"),
-                   Option("length", -1, flag: "l")) { (input: String, suffix: String, length: Int) in
+                   Option("length", -1, flag: "l")) { input, suffix, length in
     let generator = Generator(value: input)
                     print(generator.generate(suffix: suffix, maxLength: length < 0 ? nil : length))
 }
